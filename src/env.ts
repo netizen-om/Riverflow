b1b35c33fb6f921362ba4083246b06a551ca5f07
netizen-om <borisagarom@gmail.com>
2025-01-25 15:12:08 +0530

    nextjs setup

---
+++ src/env.ts
@@ -0,0 +1,8 @@
+const env = {
+    appwrite : {
+        projectId : String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+        apikey : String(process.env.APPWRITE_API_KEY),
+    }
+}
+
+export default env
