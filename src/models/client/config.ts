77c70335438ababf23dc0d9df77143e4c0a5aad9
netizen-om <borisagarom@gmail.com>
2025-01-25 21:07:12 +0530

    complete data modeling

---
+++ src/models/client/config.ts
@@ -0,0 +1,16 @@
+import env from "@/app/env";
+
+import { Client, Account , Storage , Avatars , Databases } from "appwrite";
+
+const client = new Client()
+    .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
+    .setProject(env.appwrite.projectId); // Your project ID
+
+const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+const avatars = new Avatars(client);
+
+export {client,databases,storage,avatars,account }
+
+
