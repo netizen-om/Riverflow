b29916723903ef348a17f471a445392334d8c6d0
netizen-om <borisagarom@gmail.com>
2025-01-28 20:05:36 +0530

    answer api complete

--- src/api/answer/route.ts
+++ src/api/answer/route.ts
@@ -1,5 +1,6 @@
 import { answerCollection, db } from "@/models/name";
-import { databases } from "@/models/server/config";
+import { databases, users } from "@/models/server/config";
+import { UserPrefs } from "@/store/Auth";
 import { NextRequest, NextResponse } from "next/server";
 import { ID } from "node-appwrite";
 
@@ -14,9 +15,52 @@ export async function POST(request : NextRequest){
             questionId : questionId
         })
 
+        //increase reputation
+        const prefs = await users.getPrefs<UserPrefs>(authorId)
+        await users.updatePrefs(authorId, {
+            reputation : Number(prefs.reputation) + 1
+        })
+
+        return NextResponse.json(response, {
+            status : 201
+        })
+
     } catch (error : any) {
         return NextResponse.json({
             error : error?.message || "Error creating answer"
+        },{
+            status : error?.status || error?.code || 500
+        }) 
+    }
+}
+
+export async function DELETE(request : NextRequest){
+    try {
+        const {answerId} = await request.json()
+
+        const answer = await databases.getDocument(db, answerCollection, answerId)
+
+        //delete document
+        const response = await databases.deleteDocument(db, answerCollection, answerId)
+
+        //decrese reputation
+        const prefs = await users.getPrefs<UserPrefs>(answer.authorId)
+        await users.updatePrefs(answer.authorId, {
+            reputation : Number(prefs.reputation) - 1
+        })
+
+        return NextResponse.json(
+            {
+                data : response
+            },
+            {
+                status : 200
+            }
+        )
+
+    } catch (error : any) {
+        return NextResponse.json({
+            error : error?.message || "Error Deleting answer"
         },{
             status : error?.status || error?.code || 500
         }) 
